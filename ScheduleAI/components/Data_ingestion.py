827aef5bbf383220e6eb46ea674a07f1a4a1c5a3
laiwola <layiibrahim32@gmail.com>
2025-12-23 19:38:35 +0100

    data_ingestion changes

--- ScheduleAI/components/Data_ingestion.py
+++ ScheduleAI/components/Data_ingestion.py
@@ -16,29 +16,41 @@ class DataIngestionConfig:
  dataingestionDir:str=os.path.join(artifactConfig.artifactdir,"Dataingestion")
  instruction_file:str=os.path.join(dataingestionDir,"instruction.json")
  preference_file:str=os.path.join(dataingestionDir,"preference.json")
- raw_file:str=os.path.join(dataingestionDir,"preference.json")
+ raw_file:str=os.path.join(dataingestionDir,"rawdata.json")
 
 
 class DataIngestion:
  def __init__(self,MONGO_URL):
   self.URL=MONGO_URL
   self.COLLECTION_ARR=["Instruction","","Prefrence","rawdata"]
+  self.client=MongoDBOp(MONGO_URL=self.URL,DB_NAME="AiScheduler")
+ def remove_id_field(self,data):
+  try:
+   for record in data:
+    if '_id' in record:
+     del record['_id']
+   return data
+  except Exception as e:
+   raise ErrorException(e,sys)
  def initiate_data_ingestion(self,):
   try:
+   os.makedirs(DataIngestionConfig.dataingestionDir,exist_ok=True)
    logging.info("entered data ingestion initiation")
    for collection in self.COLLECTION_ARR:
     logging.info(f"fetching data from collection:{collection}")
-    mongodb=MongoDBOp(MONGO_URL=self.URL,DB_NAME="AiScheduler")
     if collection=="Instruction":
-      instruction_data=list(mongodb.FetchALL(collection))
+      instruction_data=list(self.client.FetchALL(collection))
+      instruction_data=self.remove_id_field(instruction_data)
       with open(DataIngestionConfig.instruction_file,"w") as f:
         json.dump(instruction_data,f,indent=4)
     if collection=="Prefrence":
-      preference_data=list(mongodb.FetchALL(collection))
+      preference_data=list(self.client.FetchALL(collection))
+      preference_data=self.remove_id_field(preference_data)
       with open(DataIngestionConfig.preference_file,"w") as f:
         json.dump(preference_data,f,indent=4)
     if collection=="rawdata":
-      raw_data=list(mongodb.FetchALL(collection))
+      raw_data=list(self.client.FetchALL(collection))
+      raw_data=self.remove_id_field(raw_data)
       with open(DataIngestionConfig.raw_file,"w") as f:
         json.dump(raw_data,f,indent=4)
     logging.info(f"data ingestion completed for collection:{collection}")
--- demo.py
+++ demo.py
@@ -1,7 +1,8 @@
+import os
 from ScheduleAI.components.Data_ingestion import DataIngestion
 from dotenv import load_dotenv
-import os
+
 load_dotenv()
-URL=os.getenv("MONGO_URL")
+URL=os.getenv("MONGODB_URL")
 data_ingestion=DataIngestion(MONGO_URL=URL)
 data_ingestion.initiate_data_ingestion()
